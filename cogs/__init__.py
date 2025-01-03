7cbbd7765b0d4a218f3d2e92cf6f014b00486971
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-03 23:43:49 +0700

    Delete cogs directory

--- cogs/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from discord.ext import commands
-
-async def setup(bot: commands.Bot):
-    # วนลูปโหลดไฟล์ .py ทั้งหมดในโฟลเดอร์ cogs
-    for filename in os.listdir(os.path.dirname(__file__)):
-        if filename.endswith(".py") and filename != "__init__.py":
-            await bot.load_extension(f"cogs.{filename[:-3]}")
