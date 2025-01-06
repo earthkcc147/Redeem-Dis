1eecaac649a51560066f7ec7e7fa1ddb01c96183
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-07 01:15:31 +0700

    Update obf_vip1.py

--- obf_vip1.py
+++ obf_vip1.py
@@ -353,6 +353,8 @@ class ObfuscationView(discord.ui.View):
 
     @discord.ui.button(label="เติมเงิน", style=discord.ButtonStyle.primary)
     async def gift_link_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # ตรวจสอบว่าโฟลเดอร์และไฟล์มีอยู่แล้วหรือไม่ ถ้าไม่ให้สร้าง
+        load_data(str(interaction.guild.id))  # จะทำให้โฟลเดอร์และไฟล์ถูกสร้างขึ้นถ้ายังไม่มี
         # เปิด Modal สำหรับกรอก Gift Link
         await interaction.response.send_modal(GiftLinkModal(group_id=str(interaction.guild.id)))
     
